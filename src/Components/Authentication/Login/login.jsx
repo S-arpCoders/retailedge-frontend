93b14a19e7bbf4a0fece27879054f029ed66aa42
capaciti <capaciti>
2025-03-06 14:48:51 +0200

    Added the Login form for users to login and gain access to the dashboard

---
+++ src/Components/Authentication/Login/login.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import './login.css';
+
+const LoginForm = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        businessNumber: '',
+        email: '',
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Form Submitted:', formData);
+    };
+
+    return (
+        <div className="container_">
+            <h2 className="heading">Business Verification Form</h2>
+            <form onSubmit={handleSubmit} className="form">
+                <div className="formGroup">
+                    <label htmlFor="username" className="label">Username</label>
+                    <input
+                        type="text"
+                        id="username"
+                        name="username"
+                        value={formData.username}
+                        onChange={handleChange}
+                        className="input"
+                        required
+                    />
+                </div>
+
+                <div className="formGroup">
+                    <label htmlFor="password" className="label">Password</label>
+                    <input
+                        type="text"
+                        id="password"
+                        name="password"
+                        value={formData.password}
+                        onChange={handleChange}
+                        className="input"
+                        required
+                    />
+                </div>
+
+
+
+
+                <div className="formGroup">
+                    <button type="submit" className="button">Login</button>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default LoginForm;
